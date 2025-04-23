0108eafcf3943de2f0b65207b21b9cd075c92e6a
adnan5509 <adnanafzal5509@gmail.com>
2025-04-23 19:31:40 +0200

    enhance login component with form value persistence and debounce handling

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,27 @@
-import { NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { afterNextRender, Component, viewChild, ViewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, NgIf],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+
+
+  private form = viewChild<NgForm>('form');
+
+  constructor() {
+    afterNextRender(() => {
+      this.form()?.valueChanges?.pipe(debounceTime(5000)).subscribe(
+        { next: (value) => localStorage.setItem('login-form-details', JSON.stringify({ 'email': value.email })) }
+      );
+    })
+  }
+
   onSubmit(formData: NgForm) {
 
     if (formData.form.invalid) {
@@ -20,5 +32,6 @@ export class LoginComponent {
     console.log("Password", formData.form.value.password);
     console.log(formData);
 
+    formData.form.reset();
   }
 }
