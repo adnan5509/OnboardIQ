0e215f992b18720ca830df77f99c3dee71f5ac79
adnan5509 <adnanafzal5509@gmail.com>
2025-04-23 16:40:43 +0200

    enhance login form validation and error handling

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,18 +1,24 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
   onSubmit(formData: NgForm) {
+
+    if (formData.form.invalid) {
+      return;
+    }
     console.log("Entered Login Details");
     console.log("Email", formData.form.value.email);
     console.log("Password", formData.form.value.password);
+    console.log(formData);
 
   }
 }
