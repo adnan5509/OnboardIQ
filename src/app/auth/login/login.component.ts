38f78cff8e4ae711e8bae4053f2c1110e23cd300
adnan5509 <adnanafzal5509@gmail.com>
2025-04-24 12:57:46 +0200

    refactor login component to remove unused form logic and simplify template

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,46 +1,9 @@
-import { afterNextRender, Component, viewChild, ViewChild } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
-import { debounceTime, timeout } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
-
-
-  private form = viewChild<NgForm>('form');
-
-  constructor() {
-    afterNextRender(() => {
-      const savedForm = localStorage.getItem('saved-login-form-details');
-      if (savedForm) {
-        const loadedFormData = JSON.parse(savedForm);
-        const loadedEmail = loadedFormData.email;
-        setTimeout(() => {
-          this.form()?.controls['email'].setValue(loadedEmail);
-        }, 1)
-      }
-
-      this.form()?.valueChanges?.pipe(debounceTime(5000)).subscribe(
-        { next: (value) => localStorage.setItem('saved-login-form-details', JSON.stringify({ 'email': value.email })) }
-      );
-    })
-  }
-
-  onSubmit(formData: NgForm) {
-
-    if (formData.form.invalid) {
-      return;
-    }
-    console.log("Entered Login Details");
-    console.log("Email", formData.form.value.email);
-    console.log("Password", formData.form.value.password);
-    console.log(formData);
-
-    formData.form.reset();
-  }
-}
+export class LoginComponent { }
