0c0bbf8f0c9642a2cf086ab7687b59ac5ae3df0e
adnan5509 <adnanafzal5509@gmail.com>
2025-04-24 17:02:23 +0200

    implement reactive form for login component with email and password controls

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
+  imports: [ReactiveFormsModule],
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent { }
+export class LoginComponent {
+
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
+
+
+  onSubmit() {
+    console.log(this.loginForm);
+    console.log('Entered login Details ', this.loginForm.value.email, this.loginForm.value.password);
+  }
+
+}
