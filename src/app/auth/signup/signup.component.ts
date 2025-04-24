3c792bf45c5280d6afbc4d97135e10302f675722
adnan5509 <adnanafzal5509@gmail.com>
2025-04-25 01:30:18 +0200

    implement reactive forms in signup component with reset functionality

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
+export class SignupComponent {
+
+
+
+  signupForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  })
+  onSubmit() {
+    console.log('Email and Password : ', this.signupForm.controls.email.value, this.signupForm.controls.password.value);
+    console.log(this.signupForm);
+  }
+
+  reset() {
+    this.signupForm.reset();
+  }
+
+}
