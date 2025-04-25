c6363a9f8825016c7a520623d873fb1c1e3ae0a9
adnan5509 <adnanafzal5509@gmail.com>
2025-04-25 14:56:28 +0200

    refactor signup component to dynamically generate acquisition source checkboxes

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,3 +1,4 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { first } from 'rxjs';
@@ -5,11 +6,22 @@ import { first } from 'rxjs';
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgFor],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
 export class SignupComponent {
+
+  sourceOptions = [
+    { id: 'google', label: 'Google' },
+    { id: 'linkedin', label: 'LinkedIn' },
+    { id: 'facebook', label: 'Facebook' },
+    { id: 'newsletter', label: 'Newsletter' },
+    { id: 'friend', label: 'Referred by friend' },
+    { id: 'event', label: 'Event / Conference' },
+    { id: 'other', label: 'Other' }
+  ];
+
 
   signupForm = new FormGroup({
     email: new FormControl('', {
@@ -46,11 +58,9 @@ export class SignupComponent {
     role: new FormControl<'student' | 'teacher' | 'employee' | 'founder' | 'other'>('student', {
       validators: [Validators.required]
     }),
-    source: new FormArray([
-      new FormControl(false),
-      new FormControl(false),
-      new FormControl(false)
-    ]),
+    source: new FormArray(
+      this.sourceOptions.map(() => new FormControl(false))
+    ),
     agree: new FormControl(false, {
       validators: [Validators.required]
     })
